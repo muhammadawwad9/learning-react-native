ae27de57d3d4ccd1689a82f85fc0374d007c2de5
Muhammad Awwad <hammode.awad@gmail.com>
2021-07-12 15:17:35 +0300

    quick fix

--- utils/fetchFunc.js
+++ utils/fetchFunc.js
@@ -1,6 +1,7 @@
 const fetchFunc = async (url, options) => {
   try {
-    const local = "https://6aaf23789fcf.ngrok.io/";
+    const local =
+      "https://6aaf23789fcf.ngrok.io/"; /*here I used ngrok (ngrok allows me to expose a web server running on my local machine), so I can send requests to local servers from my mobile device*/
     const response = await fetch(`${local}${url}`, {
       ...options,
     });
